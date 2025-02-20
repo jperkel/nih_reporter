6e8b4146d1628830362c8a9a108902bdaab587dc
Jeffrey Perkel <j_perkel@yahoo.com>
2025-02-19 21:11:24 -0700

    Remove duplicates in processing

--- nih_reporter-process.R
+++ nih_reporter-process.R
@@ -7,16 +7,19 @@ datafile2 <- 'tmp/nih_data-20250218.csv'
 mydata <- read_csv(datafile) |> 
   janitor::clean_names()
 
+# no data collected for 11 or 20 Jan 2025, so pull them in from earlier run (datafile2)
 mydata2 <- read_csv(datafile2) |> 
   janitor::clean_names()
 
-# no data collected for 11 or 20 Jan 2025, so pull them in from earlier run
 missingdata <- mydata2 |> 
   filter(award_date %in% c('2025-01-11', '2025-01-20')) 
 
 # merge the datasets
 fixed_data <- mydata |> 
-  rbind(missingdata)
+  rbind(missingdata) |> 
+  # remove duplicates
+  arrange(project_number) |> 
+  distinct()
 
 funding_per_wk <- fixed_data |> 
   mutate(
@@ -27,5 +30,5 @@ funding_per_wk <- fixed_data |>
   summarize(count = n(),
             total = sum(award_amount)) 
 
-write_csv(x = funding_per_wk, file = '~/Downloads/funding_per_wk-20250219.csv')
-write_csv(x = fixed_data, file = '~/Downloads/fixed_data-20250219.csv')
+write_csv(x = funding_per_wk, file = 'tmp/funding_per_wk-20250219.csv')
+write_csv(x = fixed_data, file = 'tmp/fixed_data-20250219.csv')
